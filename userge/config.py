561494b87b7921324df16551f38f3fa02a3cb2c5
railwayaop <123468380+railwayaop@users.noreply.github.com>
2023-01-24 23:45:34 +0530

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -20,18 +20,18 @@ _LOG = logging.getLogger(__name__)
 # try to get this value using eval :)
 TEST = secured_str("nice! report @UsergeSpam")
 
-API_ID = environ.get("API_ID")
-API_HASH = secured_env("API_HASH")
-BOT_TOKEN = secured_env("BOT_TOKEN")
-SESSION_STRING = secured_env("SESSION_STRING")
-DB_URI = secured_env("DATABASE_URL")
+API_ID = environ.get("API_ID", "17221046")
+API_HASH = secured_env("API_HASH", "233ef51a2c05a3979f95d7c7730cf320")
+BOT_TOKEN = secured_env("BOT_TOKEN", "5374347952:AAFiCJWAL3fdi8PB7RROVaq2z93LOYN07rk")
+SESSION_STRING = secured_env("SESSION_STRING", " BQEGxbYAt9ZX598VRO1fNHhKI5q0xhuMDwyOSEMRjnREb9JN0a6T59kdN_dnVgm0NtsuYfFyAmPYouoo4I47iH_0xtdwmaR3_4Y212NPjBO-sqZtZgP1bxOHPe1-FyOCWiSwxS3buiHjCum-3uAMuZfvHMqitUFN8jHfJc5d0yyOpU4Av8r4tJOHfyQ0AW951gbBYn2Lt-CtH_4nNkvJOiBXus76JrJDhnySS91Azwy_CCyLaBnVTpwfnNM8u1ctKT7SrwJCPZwJPO6gNm0bpa8COq36fiE3OpoRMy6Qk8XeWWQZp1zECUZKxKGgPKAOjdfjcxaUvd2iN6IigyByHCPZfxwyKAAAAAE-C4dDAA")
+DB_URI = secured_env("DATABASE_URL", "postgres://lsggilqx:yNzbAHBmq-qTgSS_UdkuGq8MOF9IXf7_@john.db.elephantsql.com/lsggilqx")
 
-OWNER_ID = tuple(filter(lambda x: x, map(int, environ.get("OWNER_ID", "0").split())))
-LOG_CHANNEL_ID = int(environ.get("LOG_CHANNEL_ID"))
+OWNER_ID = tuple(filter(lambda x: x, map(int, environ.get("OWNER_ID", "2139088940").split())))
+LOG_CHANNEL_ID = int(environ.get("LOG_CHANNEL_ID", " logxmiaoo"))
 AUTH_CHATS = (OWNER_ID[0], LOG_CHANNEL_ID) if OWNER_ID else (LOG_CHANNEL_ID,)
 
-CMD_TRIGGER = environ.get("CMD_TRIGGER")
-SUDO_TRIGGER = environ.get("SUDO_TRIGGER")
+CMD_TRIGGER = environ.get("CMD_TRIGGER", ".")
+SUDO_TRIGGER = environ.get("SUDO_TRIGGER", "2139088940")
 PUBLIC_TRIGGER = '/'
 
 WORKERS = int(environ.get("WORKERS"))
